b0562efe3e8df8095d35fcd655e642451a3270fb
Gaurav Bharat <gaurav.mendse@gmail.com>
2024-03-05 10:19:57 +0530

    updated landing page text

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,12 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Accounts.</strong> This is the example for the{' '}
+            <strong>Welcome to example Accounts SaaS</strong>
+            {/* This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel. */}
           </p>
           <Link
             href="/login"
