5c705882c79e012d607286702a770cd2c9b5b5a5
Gaurav Bharat <gaurav.mendse@gmail.com>
2024-03-05 13:46:26 +0530

    fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,9 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to example Accounts' SaaS</strong>
+            <strong>
+              Welcome to Accounts&apos; SaaS, an example Nextjs app
+            </strong>
             {/* This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
